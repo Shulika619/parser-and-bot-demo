54c397dd652c23552afe6e2fee08d2435290149b
Sergey Shulika <shulika619@gmail.com>
2023-12-22 22:36:47 +0300

    added properties and logging

---
+++ src/main/java/dev/shulika/parserandbotdemo/config/ParserProperties.java
@@ -0,0 +1,24 @@
+package dev.shulika.parserandbotdemo.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "parser")
+public class ParserProperties {
+
+    @Value("${parser.site-name}")
+    private String siteName;
+
+    @Value("${parser.site-url}")
+    private String siteUrl;
+
+    @Value("${parser.task-cron}")
+    private String taskCron;
+
+}
